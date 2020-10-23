99b4373f78905b6056a8b8ff6faee11115163b86
Bas Dijkstra <bas@ontestautomation.com>
2020-10-23 12:21:52 +0200

    Initial commit

---
+++ src/test/java/seleniumexercises/Exercises.java
@@ -0,0 +1,39 @@
+package seleniumexercises;
+
+import seleniumexercises.pages.*;
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Exercises {
+
+    private WebDriver driver;
+
+    @Before
+    public void startBrowser() {
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void searchForVisa() {
+
+        new HomePage(driver)
+                .load()
+                .selectMenuItem("Visa");
+
+        new SearchForVisaPage(driver)
+                .setCountryOfOriginTo("American Samoa");
+
+        new VisaApplicationPage(driver);
+    }
+
+    @After
+    public void stopBrowser() {
+
+        driver.quit();
+    }
+}
